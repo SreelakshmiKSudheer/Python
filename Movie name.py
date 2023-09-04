44f9051a173edb97e1157b12f7d7dd7868de2a5c
Sreelakshmi K <119166968+SreelakshmiKSudheer@users.noreply.github.com>
2023-09-04 19:01:06 +0530

    Update Movie name.py

--- Movie name.py
+++ Movie name.py
@@ -17,23 +17,50 @@ def play():
     willing = 1
     while willing:
         
-        secret = random.choice(movies)
-        
         
         
         if(turn % 2 == 0):
            #player 1
            print(p1,", its your turn")
+           picked_movie = random.choice(movies)
            qn = create_question(picked)
            print(qn)
+           modified_qn = qn
            
-           not_said = 1 
+           not_said = True 
            while not_said:
                letter = input("Your letter: ")
                if(is_present(letter,picked_movie)):
                    #unlock
+                   modified_qn = unlock(modified_qn,picked_movie,ch)
+           		print(modified_qn)
+           		
+           		decision = input("Press 1 to guess the movie or 2 to unlock the next character")
+           		
+           		if decision  == 1:
+           		              ans = input("Enter the movie")
+           		              if ans == picked_movie:
+           		              	pp1 += 1
+           		              	print("Correct")
+           		              	not_said = False
+           		              	print(p1, " your score = ",pp1)
+           		              else: 
+           		              	print("Wrong answer. Try again")
+           		              	
+                           
                else:
                 print(letter," not found")
+			c = input("Press 1 to continue or 2 to quit")
+			
+			if c == 0:
+				 wiling = False
+			     printp1," your score : ",pp1)
+			     print(p2, " your score = ",pp2)
+				 print("Thanks for playing.\n Have a nice day")
+				
+			 
+				
         else:
             #player 2
             print(p2,", its your turn")
+		turn += 1            
