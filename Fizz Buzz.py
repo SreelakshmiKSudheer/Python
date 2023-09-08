add122ea25f4907949335867ad610f66ca155f7d
Sreelakshmi K <119166968+SreelakshmiKSudheer@users.noreply.github.com>
2023-09-08 20:45:21 +0530

    Create Fizz Buzz.py

---
+++ Fizz Buzz.py
@@ -0,0 +1,10 @@
+final = int(input("Upto what number you wanna play this FIZZ BUZZ game: "))
+for count in range(1,final + 1):
+    if count % 3 == 0 and count % 5 == 0:
+        print(str(count) + " = FIzz Buzz")
+    elif count % 3 == 0:
+        print(str(count) + " = Fizz")
+    elif count % 5 == 0:
+        print(str(count) + " = Buzz")
+    else:
+        print(count)
